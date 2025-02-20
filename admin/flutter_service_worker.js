c1c6531ca9c82a7867213474c44f5f9c6f8129ac
양소율 <yangsoyul0810@gmail.com>
2025-02-20 09:47:06 +0900

    /manager/

--- admin/flutter_service_worker.js
+++ admin/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "df9f331d362ce4ca7151a7eeff8f6fd9",
+const RESOURCES = {"flutter_bootstrap.js": "545f27e1bd3a9f60dba8e70286d632a1",
 "version.json": "e9eb58db72d407be27e9fa052224c304",
-"index.html": "80530e247c7130d3c67edc90f248a16d",
-"/": "80530e247c7130d3c67edc90f248a16d",
+"index.html": "8a95f12f50287a26cd537a94d1119dfb",
+"/": "8a95f12f50287a26cd537a94d1119dfb",
 "main.dart.js": "dcbd709f96cfbea19ddeac19fe2aa64d",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "30faa72f0b92500ed2b5499d4e7cbf7b",
