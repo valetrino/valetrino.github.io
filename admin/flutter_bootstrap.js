690bfd86609c20ab0fe3ebc3332e86e5ef53b767
양소율 <yangsoyul0810@gmail.com>
2025-03-04 07:19:21 +0900

    250303

--- admin/flutter_bootstrap.js
+++ admin/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2462687886"
+    serviceWorkerVersion: "3518466361"
   }
 });
