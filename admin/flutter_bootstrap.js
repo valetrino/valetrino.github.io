fb018ca598992205cb9d70502c51c43c8d28e866
양소율 <yangsoyul0810@gmail.com>
2025-03-04 11:33:35 +0900

    250304

--- admin/flutter_bootstrap.js
+++ admin/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3518466361"
+    serviceWorkerVersion: "62501809"
   }
 });
--- admin/flutter_service_worker.js
+++ admin/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6da83856194325d62537c836430d1640",
+const RESOURCES = {"flutter_bootstrap.js": "e8ae6920e8505bac7889bff9cfa9c101",
 "version.json": "e9eb58db72d407be27e9fa052224c304",
 "index.html": "80530e247c7130d3c67edc90f248a16d",
 "/": "80530e247c7130d3c67edc90f248a16d",
-"main.dart.js": "f199eb6a47e234ca0516e97decd6a45d",
+"main.dart.js": "7ab74f8028b40d6a5f17bf0eb0718247",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "30faa72f0b92500ed2b5499d4e7cbf7b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
